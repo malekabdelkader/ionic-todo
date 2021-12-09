532a8f857a1ec35dd58f54b353477c672f352bf1
Migouda-linux <malekabdelkader2@gmail.com>
2021-12-09 01:32:12 +0100

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBy6dKJ9y55Lc4PI9t2ZYpch6q1n6cUlH0",
+    authDomain: "my-app-349f0.firebaseapp.com",
+    databaseURL: "https://my-app-349f0-default-rtdb.firebaseio.com",
+    projectId: "my-app-349f0",
+    storageBucket: "my-app-349f0.appspot.com",
+    messagingSenderId: "460321058343",
+    appId: "1:460321058343:web:5e83e3c14d2befec62e802",
+    measurementId: "${config.measurementId}"
+  }
 };
